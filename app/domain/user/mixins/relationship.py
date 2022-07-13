7856b630c1645af7af678586013786e02a5e850e
Sebastian Salinas <seba.salinas.delrio@gmail.com>
2022-07-13 04:04:15 -0400

    user domain: RelationshipMixin edited and added

--- app/domain/user/mixins/relationship.py
+++ app/domain/user/mixins/relationship.py
@@ -1,25 +1,34 @@
-from typing import Optional
+from typing import Iterator, Optional
 
-#from ...finances.mixins.charge import ChargeMixin, PaymentPlanMixin
-#from ...shared.mixins.address import AddressMixin
+from ...account.mixins.account import AccountMixin
+from ...finance.mixins.charge import ChargeMixin
+from ...finance.mixins.renegotiation import RenegotiationMixin
+from ...shared.mixins.address import AddressMixin
 
 
-#class RelationshipMixin:
-#    
-#    address: Optional[AddressMixin] = None
-#    charges: list[ChargeMixin] = None
-#    payment_plans: list[PaymentPlanMixin] = None
-#    
-#    
-#    def charges_pending(self) -> list[ChargeMixin]:
-#        return [ charge for charge in self.charges if not charge.is_completed and not charge.nulled ]
-#
-#    def charges_completed(self) -> list[ChargeMixin]:
-#        return [ charge for charge in self.charges if charge.is_completed ]
-#
-#    def total_debt(self) -> int:
-#        return sum([charge.amount_to_be_paid() for charge in self.charges_pending()])
-#
-#    def current_debt(self) -> int:
-#        return sum([charge.current_amount_to_be_paid() for charge in self.charges_pending()])
+class RelationshipMixin:
+    
+    address: Optional[AddressMixin] = None
+    charges: list[ChargeMixin] = []
+    renegotiations: list[RenegotiationMixin] = []
+    
+    incorporation_charge: Optional[ChargeMixin] = None
+    accounts: list[AccountMixin]
+    
+    def pending_charges(self) -> Iterator[ChargeMixin]:
+        yield from filter(lambda charge: charge.is_pending, self.charges)
+        
+    def pending_renegotiations(self) -> Iterator[RenegotiationMixin]:
+        yield from filter(lambda renegotiation: renegotiation.is_pending, self.renegotiations)
 
+    def debt(self) -> int:
+        """Adds charges amount to be paid and renegotiation amount to be paid"""
+        atbp_charges = sum([charge.debt() for charge in self.pending_charges()])
+        atbp_renegotiations = sum([renegotiation.debt() for renegotiation in self.pending_renegotiations()])
+        return atbp_charges + atbp_renegotiations
+
+    def current_debt(self) -> int:
+        """Adds charges amount to be paid and renegotiation current amount to be paid"""
+        ob_charge = sum([charge.amount_to_be_paid() for charge in self.pending_charges()])
+        ob_renegotiation = sum([renegotiation.current_debt() for renegotiation in self.pending_renegotiations()])
+        return ob_charge + ob_renegotiation
--- app/domain/user/mixins/user.py
+++ app/domain/user/mixins/user.py
@@ -1,7 +1,8 @@
 from .ability import AbilityMixin
 from .identity import IdentitiyMixin
 from .auth import AuthMixin
+from .relationship import RelationshipMixin
 from ...shared.mixins.base import BaseMixin
 
-class UserMixin(AuthMixin, AbilityMixin, IdentitiyMixin, BaseMixin):
+class UserMixin(IdentitiyMixin, AuthMixin, AbilityMixin, RelationshipMixin, BaseMixin):
     """Mixin Class for user behavior"""
