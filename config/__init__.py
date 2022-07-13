fb0b058991347dba630bdf9008ea1f248a7b87b4
Sebastian Salinas <seba.salinas.delrio@gmail.com>
2022-07-13 04:05:40 -0400

    Added mail information and others to config base class

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from typing import Literal
 import os
 
@@ -10,8 +11,23 @@ class Config:
     """Base configuration object."""
     # ---------- Platform Information
     APP_NAME = os.environ.get("APP_NAME", "app").lower()
+    APP_DOMAIN = os.environ.get('APP_DOMAIN', 'app.com')
+    APP_ADMIN = os.environ.get('APP_ADMIN', 'seba.salinas.delrio@gmail.com')
+    # ---------- Flask-Mailing Configuration
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'Username')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'Password')
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 587
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_FROM = f'administracion@{APP_DOMAIN}'
+    MAIL_FROM_NAME = os.environ.get('APP_NAME')
+    DONT_REPLY_MAIL_FROM = f'error@{APP_DOMAIN}'
+    
+    
     # ---------- Flask Secret Key
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Really Hard Password')
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'Really Hard Password')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(Config):
