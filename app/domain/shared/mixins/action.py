f718e1956ba08926268cf97801db043eb4b2bf80
Sebastian Salinas <seba.salinas.delrio@gmail.com>
2022-07-13 04:03:27 -0400

    shared dom: dataclass removed from ActionMixin

--- app/domain/shared/mixins/action.py
+++ app/domain/shared/mixins/action.py
@@ -1,16 +1,12 @@
-from dataclasses import dataclass, field
-
-
 from .base import BaseMixin
-from ...users.mixins.user import UserMixin
+from ...user.mixins.user import UserMixin
 
 
-@dataclass
 class ActionMixin(BaseMixin):
     
     owner: UserMixin # User Whos making the action
     receptor: UserMixin # User Whos making the action
-    payload: dict[str, str] = field(default_factory=dict)
+    payload: dict[str, str]
 
         
         
